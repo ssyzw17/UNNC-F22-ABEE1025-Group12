619be176a4adbf17710d46c59458c498806f13e1
ssyzw17 <113959661+ssyzw17@users.noreply.github.com>
2023-02-17 15:50:20 +0800

    Add files via upload

---
+++ cw3_parametric_simulation.py
@@ -0,0 +1,49 @@
+
+#Author:Ziyu Wang,Su Li
+#Date:2023-2-13
+
+import json
+import os
+from StaticEplusEngine import run_eplus_model, convert_json_idf
+
+def run_two_parameter_simulation_helper(eplus_run_path, idf_path, output_dir, parameter_key_a, parameter_val_a,
+                                        parameter_key_b, parameter_val_b):
+    # convert idf to a JSON dict
+    convert_json_idf(eplus_run_path, idf_path)
+    eplus_json_path = idf_path.split('.idf')[0] + '.epJSON'
+    with open(eplus_json_path) as eplus_JSON:
+        eplus_json_dict = json.load(eplus_JSON)
+
+    # change the dict json val
+    inner_dict = eplus_json_dict
+    for i in range(len(parameter_key_a)):
+        if i < len(parameter_key_a) - 1:
+            inner_dict = inner_dict[parameter_key_a[i]]
+    inner_dict[parameter_key_a[-1]] = parameter_val_a
+    for j in range(len(parameter_key_b)):
+        if j < len(parameter_key_b) - 1:
+            inner_dict = inner_dict[parameter_key_b[j]]
+    inner_dict[parameter_key_b[-1]] = parameter_val_b
+
+    # put the JSON dict into idf
+    with open(eplus_json_path, 'w') as epjson:
+        json.dump(eplus_json_dict, epjson)
+    convert_json_idf(eplus_run_path, eplus_json_path)
+
+    # run simulation
+    run_eplus_model(eplus_run_path, idf_path, output_dir)
+    return output_dir
+
+
+def run_two_parameter_simulation(eplus_run_path, idf_path, output_dir, parameter_key_a, parameter_val_a,
+                                 parameter_key_b, parameter_val_b):
+    output = {}
+    os.makedirs(output_dir)
+    for i in range(len(parameter_val_a)):
+        for j in range(len(parameter_val_b)):
+            name = str(parameter_val_a[i]) + '_' + str(parameter_val_b[j])
+            output_path = output_dir + os.sep + name
+            run_two_parameter_simulation_helper(eplus_run_path, idf_path, output_path, parameter_key_a, parameter_val_a[i],
+                                                parameter_key_b, parameter_val_b[j])
+            output[name] = output_path + os.sep + 'eplusout.csv'
+    return output
---
+++ two_para_simulation.py
@@ -0,0 +1,101 @@
+#Author:Ziyu Wang,Su Li
+#Date:2023-2-14
+
+import pandas as pd
+from cw3_parametric_simulation import run_two_parameter_simulation
+
+
+class TwoParaSimulation():
+    def __init__(self, eplus_run_path, idf_path, out_put_dir):
+        self.plot_column_name = 'ZONE ONE:Zone Mean Air Temperature [C](TimeStep) '
+        self.eplus_run_path = eplus_run_path
+        self.idf_path = idf_path
+        self.out_put_dir = out_put_dir
+        self.interval = 0.02
+        self.val_start_a = None
+        self.val_end_a = None
+        self.val_start_b = None
+        self.val_end_b = None
+        self.para_key_a = None
+        self.para_key_b = None
+        self.highest_value = None
+        self.simulation_result = None
+        self.result_value = None
+
+    @property
+    def interval(self):
+        return self.interval
+
+    @interval.setter
+    def interval(self, value):
+        self._interval = value
+
+    @property
+    def val_start_a(self):
+        return self.start_a
+
+    @val_start_a.setter
+    def val_start_a(self, value):
+        self._start_a = value
+
+    @property
+    def val_end_a(self):
+        return self.val_end_a
+
+    @val_end_a.setter
+    def val_end_a(self, value):
+        self.val_end_a = value
+
+    @property
+    def val_start_b(self):
+        return self.val_start_b
+
+    @val_start_b.setter
+    def val_start_b(self, value):
+        self.val_start_b = value
+
+    @property
+    def val_end_b(self):
+        return self.val_end_b
+
+    @val_end_b.setter
+    def val_end_b(self, value):
+        self._val_end_b = value
+
+    @property
+    def para_key_a(self):
+        return self.para_key_a
+
+    @para_key_a.setter
+    def para_key_a(self, value):
+        self._para_key_a = value
+
+    @property
+    def para_key_b(self):
+        return self.para_key_b
+
+    @para_key_b.setter
+    def para_key_b(self, value):
+        self._para_key_b = value
+
+    def run_two_para_simulation(self):
+        results = {}
+        n1 = int((self.val_end_a - self.val_start_a) / self.interval) + 1
+        n2 = int((self.val_end_b - self.val_start_b) / self.interval) + 1
+        val_a = []
+        for i in range(n1):
+            val_a.append(self.val_start_a + self.interval * i)
+        val_b = []
+        for i in range(n2):
+            val_b.append(self.val_start_b + self.interval * i)
+        out_path = run_two_parameter_simulation(self.eplus_run_path, self.idf_path, self.out_put_dir,
+                                                self._para_key_a, val_a,
+                                                self._para_key_b, val_b)
+
+        for item in out_path.keys():
+            tem_csv = pd.read_csv(out_path[item])
+            tem_val = tem_csv[self.plot_column_name].values
+            results[item] = sum(tem_val / len(tem_val))
+
+        self.simulation_result = results
+        return results
